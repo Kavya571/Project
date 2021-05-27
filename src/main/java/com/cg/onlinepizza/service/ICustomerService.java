459df387afed54c83be5ee4b3100b6a0abc221f0
krishnasirikasala <71434997+krishnasirikasala@users.noreply.github.com>
2021-05-27 12:29:53 +0530

    Update ICustomerService.java

--- src/main/java/com/cg/onlinepizza/service/ICustomerService.java
+++ src/main/java/com/cg/onlinepizza/service/ICustomerService.java
@@ -1,124 +1,50 @@
-/*
- * Copyright 2008-2020 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.springframework.data.repository;
+package com.cg.onlinepizza.service;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-/**
- * Interface for generic CRUD operations on a repository for a specific type.
- *
- * @author Oliver Gierke
- * @author Eberhard Wolff
- */
-@NoRepositoryBean
-public interface CrudRepository<T, ID> extends Repository<T, ID> {
+import com.cg.onlinepizza.exceptions.CustomerIdNotFoundException;
+import com.cg.onlinepizza.repository.ICustomerRepository;
+import com.cg.onlinepizza.dto.Customer;
 
-	/**
-	 * Saves a given entity. Use the returned instance for further operations as the save operation might have changed the
-	 * entity instance completely.
-	 *
-	 * @param entity must not be {@literal null}.
-	 * @return the saved entity; will never be {@literal null}.
-	 * @throws IllegalArgumentException in case the given {@literal entity} is {@literal null}.
-	 */
-	<S extends T> S save(S entity);
 
-	/**
-	 * Saves all given entities.
-	 *
-	 * @param entities must not be {@literal null} nor must it contain {@literal null}.
-	 * @return the saved entities; will never be {@literal null}. The returned {@literal Iterable} will have the same size
-	 *         as the {@literal Iterable} passed as an argument.
-	 * @throws IllegalArgumentException in case the given {@link Iterable entities} or one of its entities is
-	 *           {@literal null}.
-	 */
-	<S extends T> Iterable<S> saveAll(Iterable<S> entities);
+@Service
+public class ICustomerService {
+	@Autowired
+	ICustomerRepository ICustomerRepository;
+	public Customer addCustomer(Customer customer) {
+		return ICustomerRepository.save(customer);
+	}
 
-	/**
-	 * Retrieves an entity by its id.
-	 *
-	 * @param id must not be {@literal null}.
-	 * @return the entity with the given id or {@literal Optional#empty()} if none found.
-	 * @throws IllegalArgumentException if {@literal id} is {@literal null}.
-	 */
-	Optional<T> findById(ID id);
+	public Customer updateCustomer(Customer customer) {
+		return ICustomerRepository.save(customer);
+	}
 
-	/**
-	 * Returns whether an entity with the given id exists.
-	 *
-	 * @param id must not be {@literal null}.
-	 * @return {@literal true} if an entity with the given id exists, {@literal false} otherwise.
-	 * @throws IllegalArgumentException if {@literal id} is {@literal null}.
-	 */
-	boolean existsById(ID id);
+	public int deleteCustomer(int customerId) throws CustomerIdNotFoundException{
+		try {
+			ICustomerRepository.deleteById(customerId);
+			return customerId;
+		}
+		catch(Exception e) {
+			throw new CustomerIdNotFoundException("Customer Id not present in table");
+		}
+	}
+	
+	public Customer viewCustomer1(int customerId) throws CustomerIdNotFoundException{
+		try {
+			return ICustomerRepository.findById(customerId).get();
+		}
+		catch(Exception e) {
+			throw new CustomerIdNotFoundException("Customer Id not present in table");
+		}
+	}
 
-	/**
-	 * Returns all instances of the type.
-	 *
-	 * @return all entities
-	 */
-	Iterable<T> findAll();
+	public List<Customer> viewCustomers(){
+		List<Customer> customer = new ArrayList<Customer>();
+		ICustomerRepository.findAll().forEach(customer1 -> customer.add(customer1));
+		return customer;
+	}
 
-	/**
-	 * Returns all instances of the type {@code T} with the given IDs.
-	 * <p>
-	 * If some or all ids are not found, no entities are returned for these IDs.
-	 * <p>
-	 * Note that the order of elements in the result is not guaranteed.
-	 *
-	 * @param ids must not be {@literal null} nor contain any {@literal null} values.
-	 * @return guaranteed to be not {@literal null}. The size can be equal or less than the number of given
-	 *         {@literal ids}.
-	 * @throws IllegalArgumentException in case the given {@link Iterable ids} or one of its items is {@literal null}.
-	 */
-	Iterable<T> findAllById(Iterable<ID> ids);
-
-	/**
-	 * Returns the number of entities available.
-	 *
-	 * @return the number of entities.
-	 */
-	long count();
-
-	/**
-	 * Deletes the entity with the given id.
-	 *
-	 * @param id must not be {@literal null}.
-	 * @throws IllegalArgumentException in case the given {@literal id} is {@literal null}
-	 */
-	void deleteById(ID id);
-
-	/**
-	 * Deletes a given entity.
-	 *
-	 * @param entity must not be {@literal null}.
-	 * @throws IllegalArgumentException in case the given entity is {@literal null}.
-	 */
-	void delete(T entity);
-
-	/**
-	 * Deletes the given entities.
-	 *
-	 * @param entities must not be {@literal null}. Must not contain {@literal null} elements.
-	 * @throws IllegalArgumentException in case the given {@literal entities} or one of its entities is {@literal null}.
-	 */
-	void deleteAll(Iterable<? extends T> entities);
-
-	/**
-	 * Deletes all entities managed by the repository.
-	 */
-	void deleteAll();
 }
