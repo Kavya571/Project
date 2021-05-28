37e5a0fc4625d1b1300bd27f7b1d39492c1d5c3b
harshini811 <82355640+harshini811@users.noreply.github.com>
2021-05-28 14:27:47 +0530

    updated

---
+++ src/main/java/com/cg/onlinepizza/dto/User.java
@@ -0,0 +1,56 @@
+package com.cg.onlinepizza.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
+public class User {
+	@Id
+	@GeneratedValue(strategy =GenerationType.AUTO)
+	private int userId;
+	private String userName;
+	private String password;
+	private boolean loggedIn;
+	
+	public boolean isLoggedIn() {
+		return loggedIn;
+	}
+	public User(int userId, String userName, String password, boolean loggedIn) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.password = password;
+		this.loggedIn = loggedIn;
+	}
+	public void setLoggedIn(boolean loggedIn) {
+		this.loggedIn = loggedIn;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName + ", password=" + password + ", loggedIn=" + loggedIn
+				+ "]";
+	}
+	
+	
+}
