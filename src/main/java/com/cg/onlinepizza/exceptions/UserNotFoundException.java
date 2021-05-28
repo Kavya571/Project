acffca55056706c5f9f79b5f3fcd728937f074ae
harshini811 <82355640+harshini811@users.noreply.github.com>
2021-05-28 14:25:40 +0530

    updated

---
+++ src/main/java/com/cg/onlinepizza/exceptions/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package com.cg.onlinepizza.exceptions;
+
+public class UserNotFoundException extends Exception {
+	public UserNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+}
