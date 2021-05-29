5e7323a003ba1d7692abb800d8aba616facd3983
Kavya17246 <48382928+Kavya17246@users.noreply.github.com>
2021-05-29 21:14:00 +0530

    updated second commit

--- src/main/java/com/cg/onlinepizza/dto/PizzaOrder.java
+++ src/main/java/com/cg/onlinepizza/dto/PizzaOrder.java
@@ -1,99 +1,102 @@
 package com.cg.onlinepizza.dto;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
 public class PizzaOrder {
 	@Id
-	private int bookingId;
+	@GeneratedValue(strategy =GenerationType.AUTO)
+	private int bookingOrderId;
 	private LocalDate dateOfOrder;
 	private String transactionMode;
 	private int quantity;
-	private String size;// medium,regular,large
-
+	private String size;//medium,regular,large
 	private double totalCost;
-
+	@OneToMany(targetEntity=Pizza.class,cascade=CascadeType.ALL)
+	@JoinColumn(name = "bookingOrderId",referencedColumnName="bookingOrderId")
+	private List<Pizza> pizza;
 	@OneToOne(cascade = CascadeType.ALL)
-	private Coupan coupan;
-	
+	private Order order;
+	@OneToOne(cascade = CascadeType.ALL)
+	private Coupan coupan;	
+	public PizzaOrder() {
+		super();
 
-//	@OneToOne(cascade = CascadeType.ALL)
-//	private Pizza pizza;
-//
-//	public Pizza getPizza() {
-//		return pizza;
-//	}
-//
-//	public void setPizza(Pizza pizza) {
-//		this.pizza = pizza;
-//	}
-
-	public int getBookingId() {
-		return bookingId;
 	}
-
-	public void setBookingId(int bookingId) {
-		this.bookingId = bookingId;
+	public PizzaOrder(int bookingOrderId, LocalDate dateOfOrder, String transactionMode, int quantity, String size,
+			double totalCost) {
+		super();
+		this.bookingOrderId = bookingOrderId;
+		this.dateOfOrder = dateOfOrder;
+		this.transactionMode = transactionMode;
+		this.quantity = quantity;
+		this.size = size;
+		this.totalCost = totalCost;
 	}
-
+	public int getBookingOrderId() {
+		return bookingOrderId;
+	}
+	public void setBookingOrderId(int bookingOrderId) {
+		this.bookingOrderId = bookingOrderId;
+	}
 	public LocalDate getDateOfOrder() {
 		return dateOfOrder;
 	}
-
 	public void setDateOfOrder(LocalDate dateOfOrder) {
 		this.dateOfOrder = dateOfOrder;
 	}
-
 	public String getTransactionMode() {
 		return transactionMode;
 	}
-
 	public void setTransactionMode(String transactionMode) {
 		this.transactionMode = transactionMode;
 	}
-
 	public int getQuantity() {
 		return quantity;
 	}
-
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
-
 	public String getSize() {
 		return size;
 	}
-
 	public void setSize(String size) {
 		this.size = size;
 	}
-
 	public double getTotalCost() {
 		return totalCost;
 	}
-
 	public void setTotalCost(double totalCost) {
 		this.totalCost = totalCost;
 	}
-
+	
+	public List<Pizza> getPizza() {
+		return pizza;
+	}
+	public void setPizza(List<Pizza> pizza) {
+		this.pizza = pizza;
+	}
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
 	public Coupan getCoupan() {
 		return coupan;
 	}
-
 	public void setCoupan(Coupan coupan) {
 		this.coupan = coupan;
-	}
-
+	} 
+	
 }
