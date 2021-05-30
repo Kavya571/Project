2935d132a2e3d313a7b57b001975a41105e02048
Kavya17246 <48382928+Kavya17246@users.noreply.github.com>
2021-05-31 05:22:42 +0530

    done

---
+++ src/main/java/com/cg/onlinepizza/exceptions/PizzaException.java
@@ -0,0 +1,13 @@
+package com.cg.onlinepizza.exceptions;
+
+public class PizzaException extends Exception {
+
+	public PizzaException() {
+		super();
+	}
+
+	public PizzaException(String message) {
+		super(message);
+	}
+	
+}
---
+++ src/main/java/com/cg/onlinepizza/exceptions/PizzaFoundException.java
@@ -0,0 +1,13 @@
+package com.cg.onlinepizza.exceptions;
+
+public class PizzaFoundException extends Exception {
+
+	public PizzaFoundException() {
+		super();
+	}
+
+	public PizzaFoundException(String message) {
+		super(message);
+	}
+
+}
