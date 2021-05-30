4177f4ffec89b37c32c8cdfaeed40664ec52e78b
harshini811 <82355640+harshini811@users.noreply.github.com>
2021-05-30 22:37:05 +0530

    Done

--- src/main/java/com/cg/onlinepizza/service/IUserService.java
+++ src/main/java/com/cg/onlinepizza/service/IUserService.java
@@ -1,49 +1,65 @@
 package com.cg.onlinepizza.service;
 
-import java.lang.StackWalker.Option;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cg.onlinepizza.repository.IUserRepository;
+import com.cg.onlinepizza.util.OnlinePizzaConstants;
 import com.cg.onlinepizza.dto.User;
 import com.cg.onlinepizza.exceptions.UserNotFoundException;
+import com.cg.onlinepizza.exceptions.ValidateUserException;
 
 @Service
 public class IUserService {
 	@Autowired
 	IUserRepository iur;
 
-	public User addNewUser(User user) {
+	public User addNewUser(User user) throws ValidateUserException {
+		validateUser(user);
 		return iur.save(user);
 	}
 
+	private boolean validateUser(User user) throws ValidateUserException {
+		if (!user.getUserName().matches("[A-Za-z0-9]+")) {
+			throw new ValidateUserException(OnlinePizzaConstants.USERNAME_CANNOT_BE_EMPTY);
+		}
+		if (!user.getPassword().matches("[A-Za-z0-9]+")) {
+			throw new ValidateUserException(OnlinePizzaConstants.PASSWORD_CANNOT_BE_EMPTY);
+		}
+		return true;
+	}
+
 	public String signin(User user) throws UserNotFoundException {
-		if (iur.existsById(user.getUserId()) && user.isLoggedIn() == false) {
-			user.setLoggedIn(true);
-			iur.save(user);
-			return "success";
-		}else {
-			return "failure"; 
+		try {
+			if (iur.existsById(user.getUserId()) && user.isLoggedIn() == false
+					&& (iur.checkpassword(user.getUserId())).equals(user.getPassword())
+					&& (iur.checkusername(user.getUserId())).equals(user.getUserName())) {
+				user.setLoggedIn(true);
+				iur.save(user);
+				return "successfully signed in";
+			}
+			return "Username or Password is Invalid";
+		} catch (Exception e) {
+			throw new UserNotFoundException("User not Exists");
 		}
 	}
 
-	public String signOut(User user) {		
-		Optional<User> u= iur.findById(user.getUserId());
-		if(u.isPresent()) {
-			User obj= u.get();
-			if(obj.isLoggedIn()==true) {
+	public String signOut(User user) {
+		Optional<User> u = iur.findById(user.getUserId());
+		if (u.isPresent()) {
+			User obj = u.get();
+			if (obj.isLoggedIn() == true) {
 				obj.setLoggedIn(false);
 				iur.save(obj);
-				return "success";
-			}
-			else {
-				return "failure";
+				return "successfully signed out";
+			} else {
+				return "failed to sign out";
 			}
 		}
-		return "failure";
-		
+		return "failed to sign out";
+
 	}
 
 	public boolean forgotPassword(int id, String oldPassword, String newPassword) throws UserNotFoundException {
@@ -52,12 +68,14 @@ public class IUserService {
 				if (oldPassword.equals(newPassword)) {
 					return false;
 				} else {
-					iur.replace(id,oldPassword, newPassword);
+					if (newPassword.matches("[A-Za-z0-9]+")) {
+						throw new ValidateUserException(OnlinePizzaConstants.PASSWORD_CANNOT_BE_EMPTY);
+					}
+					iur.replace(id, oldPassword, newPassword);
 					return true;
 				}
-			}
-			else {
-				throw new UserNotFoundException(); 
+			} else {
+				throw new UserNotFoundException();
 			}
 		} catch (Exception e) {
 			throw new UserNotFoundException("User not Exists");
