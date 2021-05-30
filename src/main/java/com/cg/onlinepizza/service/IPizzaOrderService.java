8751e445b8491f3004e0c26f60de24284192432d
Kavya571 <61652931+Kavya571@users.noreply.github.com>
2021-05-30 16:29:23 +0530

    Update IPizzaOrderService.java

--- src/main/java/com/cg/onlinepizza/service/IPizzaOrderService.java
+++ src/main/java/com/cg/onlinepizza/service/IPizzaOrderService.java
@@ -7,9 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cg.onlinepizza.dto.PizzaOrder;
+import com.cg.onlinepizza.dto.User;
 import com.cg.onlinepizza.exceptions.InvalidSizeException;
 import com.cg.onlinepizza.exceptions.OrderIdNotFoundException;
+import com.cg.onlinepizza.exceptions.UserNotFoundException;
+import com.cg.onlinepizza.exceptions.ValidatePizzaOrderException;
 import com.cg.onlinepizza.repository.IPizzaOrderRepository;
+import com.cg.onlinepizza.util.OnlinePizzaOrderConstants;
 
 
 @Service
@@ -25,12 +29,14 @@ public class IPizzaOrderService {
 		return IPizzaOrderRepository.save(orders);
 	}
 
-	public void cancelPizzaOrder(int orderId) throws OrderIdNotFoundException{
+	public int cancelPizzaOrder(int orderId) throws OrderIdNotFoundException{
 		try {
 			IPizzaOrderRepository.deleteById(orderId);
+			return orderId;
 		}catch(Exception e) {
 			throw new OrderIdNotFoundException("Order Id not present in table");
 		}
+	
 		
 	}
 	public PizzaOrder viewPizzaOrder(int orderId) throws OrderIdNotFoundException{
@@ -46,11 +52,39 @@ public class IPizzaOrderService {
 		IPizzaOrderRepository.findAll().forEach(pizzaorder1 -> pizzaorder.add(pizzaorder1));
 		return pizzaorder;
 	}
-
-//	public List<PizzaOrder> caluculateTotal(String size, int quantity) throws InvalidSizeException{
-//		List<PizzaOrder> pizzaList= caluculateTotal(size,quantity);
-//		return pizzaList;
-//		
-//	}
+	
+	public boolean ValidatePizzaOrder(PizzaOrder pizzaorder) throws ValidatePizzaOrderException {
+		if(!pizzaorder.getSize().matches("[A-Za-z]||[z-aA-Z"))
+			throw new ValidatePizzaOrderException(OnlinePizzaOrderConstants.PIZZAORDER_CANNOT_BE_EMPTY);
+				
+		if(!(pizzaorder.getQuantity()==0))
+			throw new ValidatePizzaOrderException(OnlinePizzaOrderConstants.PIZZAORDER_CANNOT_BE_EMPTY);
+				
+			return true;
+	}
+	
+	
+	public List<PizzaOrder> caluculateTotal(String size, int quantity) throws InvalidSizeException{
+		List<PizzaOrder> pizzaList= new ArrayList<PizzaOrder>(); // caluculateTotal(size,quantity);
+		return pizzaList;
+		
+	}
+	
+	public double calculateTotal(String size, int quantity) {
+		double totalCost;
+		if(size=="small") {
+			totalCost=100.00*quantity;
+			return totalCost;
+		}
+		else if(size=="medium") {
+			totalCost = 200.00*quantity;
+			return totalCost;
+		}
+		else {
+			totalCost = 300.00*quantity;
+			return totalCost;
+		}
+	
+	}
 	
 }
