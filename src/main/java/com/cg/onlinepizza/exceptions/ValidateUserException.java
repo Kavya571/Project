d365736af5b4f562be08ae89741f124f1b7d4e22
harshini811 <82355640+harshini811@users.noreply.github.com>
2021-05-30 22:23:14 +0530

    Done

---
+++ src/main/java/com/cg/onlinepizza/exceptions/ValidateUserException.java
@@ -0,0 +1,10 @@
+package com.cg.onlinepizza.exceptions;
+
+public class ValidateUserException extends Exception {
+	public ValidateUserException() {
+
+	}
+	public ValidateUserException(String message) {
+		super(message);
+	}
+}
