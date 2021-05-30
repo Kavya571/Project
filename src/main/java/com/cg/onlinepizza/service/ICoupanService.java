1536ebfbf61333f29a1644e9d3b64925560e7989
HariniChidura <82203098+HariniChidura@users.noreply.github.com>
2021-05-30 16:51:04 +0530

    Update ICoupanService.java

--- src/main/java/com/cg/onlinepizza/service/ICoupanService.java
+++ src/main/java/com/cg/onlinepizza/service/ICoupanService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.cg.onlinepizza.exceptions.CoupanIdNotFoundException;
 import com.cg.onlinepizza.exceptions.InvalidCoupanOperationException;
+import com.cg.onlinepizza.exceptions.ValidateCoupanException;
+import com.cg.onlinepizza.util.OnlinePizzaConstants;
 import com.cg.onlinepizza.dao.ICoupanRepository;
 import com.cg.onlinepizza.dto.Coupan;
 
@@ -14,9 +16,9 @@ import com.cg.onlinepizza.dto.Coupan;
 public class ICoupanService {
     @Autowired
     ICoupanRepository ICoupanRepository;
-    
-	public Coupan addCoupans(Coupan coupan) {
-		
+ 
+	public Coupan addCoupans(Coupan coupan)throws ValidateCoupanException{
+		    validateCoupan(coupan);
 		    return ICoupanRepository.save(coupan);
 			
 	};
@@ -31,6 +33,22 @@ public class ICoupanService {
 		}catch(Exception e) {
 			throw new InvalidCoupanOperationException("Operation is not valid");
 		}
+	}
+	
+	public boolean validateCoupan(Coupan coupan) throws ValidateCoupanException {
+		if (!coupan.getCoupanName().matches("[A-Za-z0-9]+")) {
+			throw new ValidateCoupanException(OnlinePizzaConstants.COUPAN_CANNOT_BE_EMPTY);
+		}
+		if (!coupan.getCoupanType().matches("[A-Za-z0-9]+"))
+			throw new ValidateCoupanException(OnlinePizzaConstants.COUPAN_CANNOT_BE_EMPTY);
+		
+		if (!coupan.getCoupanDescription().matches("[A-Za-z]+[\s][0-9]+[\s][%][a-zA-Z\s]+"))
+			throw new ValidateCoupanException(OnlinePizzaConstants.DESCRIPTION_FORMAT_NOT_MATCHED);
+		String coupanPizzaIdString = Integer.toString(coupan.getCoupanPizzaId());
+		if (!(coupanPizzaIdString.matches("[0-9]+")))
+			throw new ValidateCoupanException(OnlinePizzaConstants.COUPANPIZZAID_FORMAT);
+
+		return true;
 	}
 
 	public int deleteCoupans(int coupanId)throws CoupanIdNotFoundException{
