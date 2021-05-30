ceefaaa73c63b31a4c06b0c5f05353322781a6b5
Kavya571 <61652931+Kavya571@users.noreply.github.com>
2021-05-30 21:46:08 +0530

    Add files via upload

--- src/main/java/com/cg/onlinepizza/dto/PizzaOrder.java
+++ src/main/java/com/cg/onlinepizza/dto/PizzaOrder.java
@@ -15,88 +15,112 @@ import javax.persistence.OneToOne;
 @Entity
 public class PizzaOrder {
 	@Id
-	@GeneratedValue(strategy =GenerationType.AUTO)
-	private int bookingOrderId;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int bookingId;
 	private LocalDate dateOfOrder;
 	private String transactionMode;
 	private int quantity;
-	private String size;//medium,regular,large
-	private double totalCost;
-	@OneToMany(targetEntity=Pizza.class,cascade=CascadeType.ALL)
-	@JoinColumn(name = "bookingOrderId",referencedColumnName="bookingOrderId")
-	private List<Pizza> pizza;
-	@OneToOne(cascade = CascadeType.ALL)
-	private Order order;
-	@OneToOne(cascade = CascadeType.ALL)
-	private Coupan coupan;	
-	public PizzaOrder() {
-		super();
+	private String size;// medium,regular,large
 
-	}
-	public PizzaOrder(int bookingOrderId, LocalDate dateOfOrder, String transactionMode, int quantity, String size,
-			double totalCost) {
+	private double totalCost;
+	
+	@OneToOne(cascade= CascadeType.ALL)
+	private Order orders;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Coupan coupan;
+	
+	@OneToMany(targetEntity=Pizza.class,cascade = CascadeType.ALL)
+	@JoinColumn(name="bookingId",referencedColumnName="bookingId")
+	private List<Pizza> pizza;
+
+	
+	public PizzaOrder(int bookingId, LocalDate dateOfOrder, String transactionMode, int quantity, String size,
+			double totalCost//,Coupan coupan
+			) {
 		super();
-		this.bookingOrderId = bookingOrderId;
+		this.bookingId = bookingId;
 		this.dateOfOrder = dateOfOrder;
 		this.transactionMode = transactionMode;
 		this.quantity = quantity;
 		this.size = size;
 		this.totalCost = totalCost;
+		//this.coupan = coupan;
 	}
-	public int getBookingOrderId() {
-		return bookingOrderId;
-	}
-	public void setBookingOrderId(int bookingOrderId) {
-		this.bookingOrderId = bookingOrderId;
-	}
-	public LocalDate getDateOfOrder() {
-		return dateOfOrder;
-	}
-	public void setDateOfOrder(LocalDate dateOfOrder) {
-		this.dateOfOrder = dateOfOrder;
-	}
-	public String getTransactionMode() {
-		return transactionMode;
-	}
-	public void setTransactionMode(String transactionMode) {
-		this.transactionMode = transactionMode;
-	}
-	public int getQuantity() {
-		return quantity;
-	}
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-	public String getSize() {
-		return size;
-	}
-	public void setSize(String size) {
-		this.size = size;
-	}
-	public double getTotalCost() {
-		return totalCost;
-	}
-	public void setTotalCost(double totalCost) {
-		this.totalCost = totalCost;
+	public PizzaOrder() {
+	
 	}
 	
 	public List<Pizza> getPizza() {
 		return pizza;
 	}
+
 	public void setPizza(List<Pizza> pizza) {
 		this.pizza = pizza;
 	}
-	public Order getOrder() {
-		return order;
+	
+	public int getBookingId() {
+		return bookingId;
 	}
-	public void setOrder(Order order) {
-		this.order = order;
+
+	public void setBookingId(int bookingId) {
+		this.bookingId = bookingId;
 	}
+
+	public LocalDate getDateOfOrder() {
+		return dateOfOrder;
+	}
+
+	public void setDateOfOrder(LocalDate dateOfOrder) {
+		this.dateOfOrder = dateOfOrder;
+	}
+
+	public String getTransactionMode() {
+		return transactionMode;
+	}
+
+	public void setTransactionMode(String transactionMode) {
+		this.transactionMode = transactionMode;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public double getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(double totalCost) {
+		this.totalCost = totalCost;
+	}
+
 	public Coupan getCoupan() {
 		return coupan;
 	}
+
 	public void setCoupan(Coupan coupan) {
 		this.coupan = coupan;
-	} 
-	
+	}
+
+	public Order getOrders() {
+		return orders;
+	}
+
+	public void setOrders(Order orders) {
+		this.orders = orders;
+	}
+
 }
