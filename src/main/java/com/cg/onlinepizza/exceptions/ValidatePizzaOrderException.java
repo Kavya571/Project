53cec8b21bd61b5918be78ba2c05dab887bf3f2c
Kavya571 <61652931+Kavya571@users.noreply.github.com>
2021-05-30 21:44:17 +0530

    Add files via upload

---
+++ src/main/java/com/cg/onlinepizza/exceptions/ValidatePizzaOrderException.java
@@ -0,0 +1,11 @@
+package com.cg.onlinepizza.exceptions;
+
+public class ValidatePizzaOrderException extends Exception {
+	public ValidatePizzaOrderException() {
+		// TODO Auto-generated constructor stub
+	}
+	public ValidatePizzaOrderException(String message) {
+		super(message);
+	}
+
+}
