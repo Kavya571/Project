7fcaf86fbb5470514b2c960772b6e5adc571ce85
harshini811 <82355640+harshini811@users.noreply.github.com>
2021-05-30 22:20:39 +0530

    Done

--- src/main/java/com/cg/onlinepizza/dto/User.java
+++ src/main/java/com/cg/onlinepizza/dto/User.java
@@ -4,6 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 
@@ -14,9 +18,13 @@ public class User {
 	private String userName;
 	private String password;
 	private boolean loggedIn;
-	
+
 	public boolean isLoggedIn() {
 		return loggedIn;
+	}
+	
+	public User() {
+		super();
 	}
 	public User(int userId, String userName, String password, boolean loggedIn) {
 		super();
