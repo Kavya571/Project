dd710f0f3dca572a024fd33f316b01a4c3580df4
harshini811 <82355640+harshini811@users.noreply.github.com>
2021-05-31 18:00:19 +0530

    updated user

--- src/test/java/com/cg/onlinepizza/pizzaorder/Testuser.java
+++ src/test/java/com/cg/onlinepizza/pizzaorder/Testuser.java
@@ -9,41 +9,40 @@ import com.cg.onlinepizza.exceptions.UserNotFoundException;
 import com.cg.onlinepizza.exceptions.ValidateUserException;
 import com.cg.onlinepizza.service.IUserService;
 
-public class Testuser extends OnlinePizzaApplicationTests{
+public class Testuser extends OnlinePizzaApplicationTests {
 	@Autowired
 	IUserService ius;
+
 	@Test
 	void testIUserService() {
-	    assertTrue(ius instanceof IUserService);
+		assertTrue(ius instanceof IUserService);
 	}
 
-    @Test
+	@Test
 	void testAddNewUser() throws ValidateUserException {
-		User u = new User(15,"harshini","H@rshini811", false);
+		User u = new User(6, "vaishnav", "Vaishu@22", false);
 		u = ius.addNewUser(u);
-		assertEquals(u.getUserName(), "harshini");
+		assertEquals(u.getUserName(), "vaishnav");
 	}
-    @Test
+
+	@Test
 	void testSignIn() throws UserNotFoundException, ValidateUserException {
-		User u = new User(15,"harshini","H@rshini811", false);
-		ius.addNewUser(u);
+		User u = new User(5, "vaishnavi", "Vaishu@22", false);
 		String p = ius.signin(u);
-		assertEquals("successfully signed in",p);
+		assertEquals("successfully signed in", p);
 	}
 
 	@Test
 	void testSignOut() throws ValidateUserException {
-		User u = new User(7,"likitha","Likith@9", true);
-		ius.addNewUser(u);
+		User u = new User(5, "vaishnavi", "Vaishu@22", true);
 		String p = ius.signOut(u);
-		assertEquals("successfully signed out",p);
+		assertEquals("successfully signed out", p);
 	}
-	@Test
-	void testForgotPassword() throws UserNotFoundException {
-		boolean b = ius.forgotPassword(1, "harshini811", "harshini81");
-		assertEquals(true,b);	
-	}
-	
 
+	@Test
+	void testForgotPassword() throws UserNotFoundException, ValidateUserException {
+		boolean b = ius.forgotPassword(3, "harini", "H@rini897");
+		assertEquals(true, b);
+	}
 
 }
