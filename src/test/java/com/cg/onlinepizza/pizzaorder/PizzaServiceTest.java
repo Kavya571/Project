b52117ab481a196301659529583bc3fa7ac03901
Kavya17246 <48382928+Kavya17246@users.noreply.github.com>
2021-05-31 05:44:02 +0530

    done

--- src/test/java/com/cg/onlinepizza/pizzaorder/PizzaServiceTest.java
+++ src/test/java/com/cg/onlinepizza/pizzaorder/PizzaServiceTest.java
@@ -8,41 +8,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.cg.onlinepizza.dto.Pizza;
+import com.cg.onlinepizza.exceptions.PizzaException;
+import com.cg.onlinepizza.exceptions.PizzaFoundException;
 import com.cg.onlinepizza.exceptions.PizzaIdNotFoundException;
+import com.cg.onlinepizza.exceptions.ValidatePizzaException;
 import com.cg.onlinepizza.service.IPizzaService;
 
 public class PizzaServiceTest extends OnlinePizzaApplicationTests {
 	@Autowired
 	IPizzaService service;
+//	@Test
+//	public void testAddPizza() throws PizzaFoundException, ValidatePizzaException {
+//		Pizza newPizz=new Pizza(1,"Medium","Cheese Burst","Vegetarian",200,100);
+//		assertEquals(PizzaFoundException.class,service.addPizza(newPizz));
+//	}
 	@Test
-	public void testAddPizza() {
-		Pizza newPizz=new Pizza(1,"Medium","CheeseBurst","Vegetarian",200,100);
+	public void testAddPizza1() throws PizzaFoundException, ValidatePizzaException {
+		Pizza newPizz=new Pizza(10,"Large","Cheese Burst","Vegetarian",200,100);
 		newPizz=service.addPizza(newPizz);
-		assertEquals("CheeseBurst",newPizz.getPizzaName());
+		assertEquals("Cheese Burst",newPizz.getPizzaName());
 	}
 	@Test
-	public void testViewPizzaPizza() {
+	public void testViewPizzaPizza() throws PizzaException {
 		List<Pizza> p=service.viewPizzaList();
 		assertThat(p).size().isGreaterThan(0);
 		
 	}
 	@Test
-	public void testFindPizzaById() {
-		Pizza newPizz=new Pizza(2,"Large","Chicken Pizza","Non-Vegetarian",200,100);
+	public void testFindPizzaById() throws PizzaFoundException, ValidatePizzaException {
+		Pizza newPizz=new Pizza(15,"Large","Chicken Pizza","Non Vegetarian",200,100);
 				Pizza np=service.addPizza(newPizz);
 		
 				assertEquals(np.getPizzaType(),"Large");
 	}
 	@Test
-	public void testDeletePizzaPizza() throws PizzaIdNotFoundException{
-		Pizza np=new Pizza(2,"Medium","CheeseBurst","Vegetarian",200,100);
+	public void testDeletePizzaPizza() throws PizzaIdNotFoundException, PizzaFoundException, ValidatePizzaException{
+		Pizza np=new Pizza(16,"Medium","Cheese Burst","Vegetarian",200,100);
 		 np=service.addPizza(np);
-		 assertEquals(1,service.deletePizza(np.getPizzaId()));
+		 assertEquals(16,service.deletePizza(np.getPizzaId()));
 	}
 	@Test
-	public void testUpdatePizza() {
-		Pizza np=new Pizza(3,"Small","CheeseBurst","Vegetarian",300,400);
-		np=service.updatePizza(new Pizza(1,"Medium","CheeseBurst","Non-Vegetarian",500,600));
-		assertEquals("Non-Vegetarian",np.getPizzaDescription());
+	public void testUpdatePizza() throws PizzaIdNotFoundException, ValidatePizzaException {
+		Pizza np=new Pizza(9,"Small","Cheese Burst","Vegetarian",300,400);
+		np=service.updatePizza(new Pizza(9,"Medium","Cheese Burst","Non Vegetarian",500,600));
+		assertEquals("Non Vegetarian",np.getPizzaDescription());
 	}
 }
