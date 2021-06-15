157059bc2031e78f71c52cb4de6153ea6e592878
HariniChidura <82203098+HariniChidura@users.noreply.github.com>
2021-06-15 17:51:49 +0530

    Update coupan.component.ts

--- onlinePizza/src/app/components/coupan/coupan.component.ts
+++ onlinePizza/src/app/components/coupan/coupan.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Coupan } from 'src/app/coupan';
+import { Observable, Subject } from 'rxjs';
+import { CoupanService } from 'src/app/services/coupan.service';
 
 @Component({
   selector: 'app-coupan',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coupan.component.css']
 })
 export class CoupanComponent implements OnInit {
-
-  constructor() { }
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any>= new Subject();
+  Coupans: Observable<Coupan[]>;
+  Coupan : Coupan=new Coupan();
+  deleteMessage=false;
+  constructor(private CoupanService: CoupanService) { }
 
   ngOnInit() {
+    this.dtOptions = {
+      pageLength: 6,
+      stateSave:true,
+      lengthMenu:[[6, 16, 20, -1], [6, 16, 20, "All"]],
+      processing: true
+    };   
+    this.CoupanService.getCoupan().subscribe(data =>{
+    this.Coupans =data;
+    this.dtTrigger.next();
+    })
+  }
+  delete(id: number) {
+    this.CoupanService.delete(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.deleteMessage=true;
+          this.CoupanService.getCoupan().subscribe(data =>{
+            this.Coupans =data
+            })
+        },
+        error => console.log(error));
   }
 
 }
