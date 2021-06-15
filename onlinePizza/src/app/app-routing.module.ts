dbc8594df59ab4c1fc989d6e0669d4461536ae14
krishnasirikasala <71434997+krishnasirikasala@users.noreply.github.com>
2021-06-15 14:49:05 +0530

    Update app-routing.module.ts

--- onlinePizza/src/app/app-routing.module.ts
+++ onlinePizza/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PizzaComponent } from './components/pizza/pizza.component';
+import { CustomerComponent } from './customer/customer.component';
+
 
 
 const routes: Routes = [
-  { path: 'view-pizza', component: PizzaComponent }
+  { path: 'view-pizza', component: PizzaComponent },
+  { path: 'view-customer', component: CustomerComponent }
 ];
 
 @NgModule({
