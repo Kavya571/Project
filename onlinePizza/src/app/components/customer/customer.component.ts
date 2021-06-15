1c2a9f0fbd7ef0bcd2ee4eaf59154e2a542bb8f9
krishnasirikasala <71434997+krishnasirikasala@users.noreply.github.com>
2021-06-15 14:44:47 +0530

    Update customer.component.ts

--- onlinePizza/src/app/components/customer/customer.component.ts
+++ onlinePizza/src/app/components/customer/customer.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable, Subject } from 'rxjs';
+import { Customer } from 'src/app/customer';
+import { CustomerService } from 'src/app/services/customer.service';
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any>= new Subject();
+  Customers: Observable<Customer[]>;
+  Customer : Customer=new Customer();
+  deleteMessage=false;
 
-  constructor() { }
+  constructor(private CustomerService:CustomerService) { }
 
   ngOnInit() {
+    this.dtOptions = {
+      pageLength: 6,
+      stateSave:true,
+      lengthMenu:[[6, 16, 20, -1], [6, 16, 20, "All"]],
+      processing: true
+    };   
+    this.CustomerService.getCustomer().subscribe(data =>{
+    this.Customers =data;
+    this.dtTrigger.next();
+    })
+  }
+  delete(id: number) {
+    this.CustomerService.delete(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.deleteMessage=true;
+          this.CustomerService.getCustomer().subscribe(data =>{
+            this.Customers =data
+            })
+        },
+        error => console.log(error));
+
   }
 
 }
