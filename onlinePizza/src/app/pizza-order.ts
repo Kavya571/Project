59ce72d073cb38e3d80ae133ea8042ac9ac59274
Kavya571 <61652931+Kavya571@users.noreply.github.com>
2021-06-15 14:47:45 +0530

    Update pizza-order.ts

--- onlinePizza/src/app/pizza-order.ts
+++ onlinePizza/src/app/pizza-order.ts
@@ -1,2 +1,10 @@
 export class PizzaOrder {
+    public bookingId: number;
+    public dateOfOrder: Date;
+	public transactionMode:String;
+	public quantity:String ;
+	public size:String ;
+	public totalCost:number;
+    constructor() { }
+
 }
