e6517f4ab77dddc9c47aa2afbb761c885074950b
krishnasirikasala <71434997+krishnasirikasala@users.noreply.github.com>
2021-06-15 14:46:37 +0530

    Update customer.service.ts

--- onlinePizza/src/app/services/customer.service.ts
+++ onlinePizza/src/app/services/customer.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/internal/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
+  CUSTOMER_PATH = 'http://localhost:7072/';
+  GET_CUSTOMER_PATH = '/viewCustomer';
 
-  constructor() { }
-}
+  constructor(private http: HttpClient) { }
+  getCustomer(): Observable<any> {
+    //return this.http.get(`${this.baseUrl}`+'students-list');
+    return this.http.get(this.CUSTOMER_PATH + this.GET_CUSTOMER_PATH).pipe(map(
+      res => {
+        if (res) {
+          return res;
+        } else {
+          return {};
+        }
+      }
+    ));
+  }
+  delete(id): Observable<any> {
+    return this.http.delete(this.CUSTOMER_PATH + '/customer/'+id).pipe(map(
+      res => {
+        if (res) {
+          return res;
+        } else {
+          return {};
+        }
+      }
+      ));
+    }
+  }
