6cad97770f814e49683e7fa1140b4d3b3b9f990c
HariniChidura <82203098+HariniChidura@users.noreply.github.com>
2021-06-15 17:55:14 +0530

    Update coupan.ts

--- onlinePizza/src/app/coupan.ts
+++ onlinePizza/src/app/coupan.ts
@@ -1,2 +1,9 @@
 export class Coupan {
+    public coupanId: number;
+    public coupanName:String;
+    public coupanType:String;
+    public coupanDescription:String;
+    public coupanPizzaId:number;
+    constructor(){ }
 }
+
