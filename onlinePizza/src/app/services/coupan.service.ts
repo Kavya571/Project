feaeadfda2549738f8ba40b433c8f1334123034a
HariniChidura <82203098+HariniChidura@users.noreply.github.com>
2021-06-15 17:52:31 +0530

    Update coupan.service.ts

--- onlinePizza/src/app/services/coupan.service.ts
+++ onlinePizza/src/app/services/coupan.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/internal/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoupanService {
 
-  constructor() { }
+  COUPAN_PATH = 'http://localhost:9090/';
+  GET_COUPAN_PATH = '/viewCoupan';
+  constructor(private http: HttpClient) { }
+  getCoupan(): Observable<any> {
+
+    return this.http.get(this.COUPAN_PATH + this.GET_COUPAN_PATH).pipe(map(
+      res => {
+        if (res) {
+          return res;
+        } else {
+          return {};
+        }
+      }
+    ));
+  }
+  
+  createCoupan(coupan: object): Observable<object> {
+    return this.http.post(`${this.COUPAN_PATH}`+'coupan', coupan);
+  }
+  delete(id): Observable<any> {
+    return this.http.delete(this.COUPAN_PATH + '/coupan/'+id).pipe(map(
+      res => {
+        if (res) {
+          return res;
+        } else {
+          return {};
+        }
+      }
+    ));
+  }
 }
