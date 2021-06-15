868f3b9a115d4784b1338b398b6b5f89d289ca8f
Kavya571 <61652931+Kavya571@users.noreply.github.com>
2021-06-15 14:45:21 +0530

    Update pizza-order.component.ts

--- onlinePizza/src/app/components/pizza-order/pizza-order.component.ts
+++ onlinePizza/src/app/components/pizza-order/pizza-order.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { PizzaOrder } from 'src/app/pizza-order';
+import { PizzaOrderService } from 'src/app/services/pizza-order.service';
+
 
 @Component({
   selector: 'app-pizza-order',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pizza-order.component.css']
 })
 export class PizzaOrderComponent implements OnInit {
-
-  constructor() { }
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any>= new Subject();
+  PizzaOrders: Observable<PizzaOrder[]>;
+  PizzaOrder : PizzaOrder=new PizzaOrder();
+  deleteMessage=false;
+  constructor(private PizzaOrderService:PizzaOrderService) { }
 
   ngOnInit() {
+    this.dtOptions = {
+      pageLength: 6,
+      stateSave:true,
+      lengthMenu:[[6, 16, 20, -1], [6, 16, 20, "All"]],
+      processing: true
+    };   
+    this.PizzaOrderService.getPizzaOrder().subscribe(data =>{
+    this.PizzaOrders =data;
+    this.dtTrigger.next();
+    })
+  }
+  delete(id: number) {
+    this.PizzaOrderService.delete(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.deleteMessage=true;
+          this.PizzaOrderService.getPizzaOrder().subscribe(data =>{
+            this.PizzaOrders =data
+            })
+        },
+        error => console.log(error));
   }
 
 }
