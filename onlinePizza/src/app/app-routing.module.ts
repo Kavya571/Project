7ab4770b6da15e3108fc8ca3f17fa3dd2e2084a5
HariniChidura <82203098+HariniChidura@users.noreply.github.com>
2021-06-15 17:53:43 +0530

    Update app-routing.module.ts

--- onlinePizza/src/app/app-routing.module.ts
+++ onlinePizza/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { PizzaComponent } from './components/pizza/pizza.component';
 import { PizzaOrderComponent } from './components/pizza-order/pizza-order.component';
 import { CustomerComponent } from './customer/customer.component';
+import { AddCoupanComponent } from './components/add-coupan/add-coupan.component';
+import { CoupanComponent } from './components/coupan/coupan.component';
 
 
 
 const routes: Routes = [
   { path: 'view-pizza', component: PizzaComponent },
   { path: 'view-customer', component: CustomerComponent },
-   { path: 'view-pizzaOrder', component: PizzaOrderComponent }
+   { path: 'view-pizzaOrder', component: PizzaOrderComponent },
+  { path: 'view-coupan',component: CoupanComponent},
+  { path: 'add-coupan', component: AddCoupanComponent }
 ];
 
 @NgModule({
