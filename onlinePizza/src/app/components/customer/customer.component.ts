0e7badd5d3b3f7fa46c2bd97264a1427c4178ac4
krishnasirikasala <71434997+krishnasirikasala@users.noreply.github.com>
2021-06-19 20:46:44 +0530

    Update customer.component.ts

--- onlinePizza/src/app/components/customer/customer.component.ts
+++ onlinePizza/src/app/components/customer/customer.component.ts
@@ -13,6 +13,7 @@ export class CustomerComponent implements OnInit {
   Customers: Observable<Customer[]>;
   Customer : Customer=new Customer();
   deleteMessage=false;
+  profileMessage=false;
 
   constructor(private CustomerService:CustomerService) { }
 
@@ -27,6 +28,18 @@ export class CustomerComponent implements OnInit {
     this.Customers =data;
     this.dtTrigger.next();
     })
+  }
+  profile(id: number){
+    this.CustomerService.profile(id)
+    .subscribe(
+      data => {
+        console.log(data);
+        this.profileMessage=true;
+        this.CustomerService.getCustomer().subscribe(data =>{
+          this.Customers =data
+          })
+      },
+      error => console.log(error));
   }
   delete(id: number) {
     this.CustomerService.delete(id)
