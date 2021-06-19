658f61db8026a73d6b68a1213666d1e9bd7063f5
Kavya17246 <48382928+Kavya17246@users.noreply.github.com>
2021-06-19 10:00:26 +0530

    second comitt

--- onlinePizza/src/app/services/login.service.ts
+++ onlinePizza/src/app/services/login.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/internal/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
-  constructor() { }
+  username:string;
+  LOGIN_PATH = 'http://localhost:7070/';
+  constructor(private http: HttpClient) { }
+  signin(UserName,Password):Observable<object>{
+    return this.http.get(this.LOGIN_PATH+'/signin/'+UserName+'/'+Password).pipe(map(
+      res => {
+        if (res) {
+          return res;
+        } else {
+          return {};
+        }
+      },
+      this.username=UserName
+    ));
+  }
 }
