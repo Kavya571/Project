e13caa5e84e79d8f3a4ae0bc8557deedeefc0acf
Kavya17246 <48382928+Kavya17246@users.noreply.github.com>
2021-06-19 09:56:39 +0530

    second commit

--- onlinePizza/src/app/components/pizza/pizza.component.ts
+++ onlinePizza/src/app/components/pizza/pizza.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { Pizza } from 'src/app/pizza';
+import { Pizza } from 'src/app/classes/pizza';
+import { LoginService } from 'src/app/services/login.service';
 import { PizzaServiceService } from 'src/app/services/pizza.service';
+declare var toastr: any;
 @Component({
   selector: 'app-pizza',
   templateUrl: './pizza.component.html',
-  styleUrls: ['./pizza.component.css']
+  styleUrls: ['./pizza.component.css'],
 })
 export class PizzaComponent implements OnInit {
   dtOptions: DataTables.Settings = {};
@@ -13,8 +15,8 @@ export class PizzaComponent implements OnInit {
   Pizzas: Observable<Pizza[]>;
   Pizza : Pizza=new Pizza();
   deleteMessage=false;
-  constructor(private PizzaServiceService:PizzaServiceService) { }
-
+  userName:String;
+  constructor(private PizzaServiceService:PizzaServiceService,private LoginService:LoginService) { }
   ngOnInit() {
     this.dtOptions = {
       pageLength: 6,
@@ -26,18 +28,21 @@ export class PizzaComponent implements OnInit {
     this.Pizzas =data;
     this.dtTrigger.next();
     })
+    this.userName=this.LoginService.username;
   }
   delete(id: number) {
     this.PizzaServiceService.delete(id)
       .subscribe(
         data => {
           console.log(data);
+          toastr.success("SuccessFully deleted");
           this.deleteMessage=true;
           this.PizzaServiceService.getPizza().subscribe(data =>{
             this.Pizzas =data
             })
         },
         error => console.log(error));
+        
   }
 
 }
